4e76afb90343d0c91da94062f208d39869334462
Tom Radford <tom@radfords-web.com>
2023-01-18 18:56:40 -0400

    Lesson 8 - images SF symbols

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,23 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var messageString = ""
+    @State private var messageString = "Namaste"
     
     var body: some View {
         
         
         VStack {
                 Spacer()
-            
+            Image(systemName: "speaker.wave.2", variableValue: 0.550)
+                .resizable()
+                .scaledToFit()
+                .symbolRenderingMode(.multicolor)
+                .padding()
+                .background(Color(hue: 0.508, saturation: 0.253, brightness: 0.963))
+                .cornerRadius(30)
+                .shadow( color: .gray, radius: 30, x:20, y:20)
+                .overlay(RoundedRectangle(cornerRadius: 30)
+                    .stroke(.teal, lineWidth: 1)
+                )
+                
+                .padding()
             Text(messageString)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
-                .minimumScaleFactor(0.5)
+                .minimumScaleFactor(0.1)
                 .multilineTextAlignment(.center)
                 .foregroundColor(.red)
                 .frame( height: 150)
                 .frame(maxWidth: .infinity)
-                .border(.orange, width: 1)
+          //      .border(.orange, width: 1)
                 .padding()
             
             Spacer()
